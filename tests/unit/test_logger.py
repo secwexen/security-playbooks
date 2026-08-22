54dff03b0ec5eb4aed001f269cc005dbb662d215
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-08-22 09:46:49 +0300

    Update test_logger.py
    Signed-off-by: The Secwexen <129677392+secwexen@users.noreply.github.com>

--- tests/unit/test_logger.py
+++ tests/unit/test_logger.py
@@ -1,1 +1,9 @@
+from tools.utils.logger import get_logger
 
+
+def test_get_logger():
+    logger = get_logger("test")
+
+    assert logger.name == "test"
+    assert logger.level > 0
+    assert len(logger.handlers) == 2
